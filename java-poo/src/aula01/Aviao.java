26ba69e3c0ef1ad8804b2739ecd4e0dff5fc2edd
Gleides Vinente <gleidevelop@gmail.com>
2022-07-27 12:17:45 -0400

    Projeto Inicial:modulos java-poo e design-patterns

---
+++ java-poo/src/aula01/Aviao.java
@@ -0,0 +1,133 @@
+package aula01;
+
+public class Aviao {
+    private String modelo;
+    private String identificador;
+    private boolean motor;
+    private float altura;
+    private float velocidade;
+    private boolean emVoo;
+
+    public Aviao(String modelo, String identificador) {
+        this.setModelo(modelo);
+        ;
+        this.setIdentificador(identificador);
+        this.setMotor(false);
+        this.setAltura(0.0f);
+    }
+
+    public void atualizarStatusVoo() {
+        if (isEmVoo()) {
+            if (getVelocidade() >= 200) {
+                System.out.println("Estou voando...");
+            } else {
+                setEmVoo(false);
+                System.out.println("Estou aterrisando...");
+            }
+        } else {
+            if (getVelocidade() < 200) {
+                System.out.println("Estou em solo...");
+            }else{
+                setEmVoo(true);
+                System.out.println("Estou decolando...");
+            }
+        }
+    }
+
+    public void acelerar() {
+        if (getEstadoMotor()) {
+            setVelocidade(getVelocidade() + 50.0f);
+            System.out.println("Avião a " + getVelocidade() + " km/h");
+        } else {
+            System.out.println("ERRO: Motor desligado");
+        }
+        atualizarStatusVoo();
+    }
+
+    public void desacelerar() {
+        if (getEstadoMotor()) {
+            if (getVelocidade() > 0.0f) {
+                setVelocidade(getVelocidade() - 50.0f);
+            }
+            System.out.println("Avião a " + getVelocidade() + " km/h");
+        } else {
+            System.out.println("ERRO: Motor desligado");
+        }
+        atualizarStatusVoo();
+    }
+
+    public boolean getEstadoMotor() {
+        return isMotor();
+    }
+
+    public void imprimeEstadoMotor() {
+        if (isMotor()) {
+            System.out.println("O motor esta ligado..");
+        } else {
+            System.out.println("O motor está desligado..");
+        }
+    }
+
+    public void ligarMotor() {
+        setMotor(true);
+        System.out.println("Vrummmmmmm");
+    }
+
+    public void desligarMotor() {
+        setMotor(false);
+    }
+
+    public void imprimirOk() {
+        System.out.println("Ok");
+
+    }
+
+    public boolean isEmVoo() {
+        return emVoo;
+    }
+
+    private void setEmVoo(boolean emVoo) {
+        this.emVoo = emVoo;
+    }
+
+    public float getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(float velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public boolean isMotor() {
+        return motor;
+    }
+
+    public void setMotor(boolean motor) {
+        this.motor = motor;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+}
---
+++ java-poo/src/aula01/Main.java
@@ -0,0 +1,54 @@
+package aula01;
+
+class Main{
+    public static void main(String[] args) {
+        Aviao baraoVermelho = new Aviao("Fooker DR1", "Freitherr");
+        baraoVermelho.imprimirOk();
+
+        baraoVermelho.setMotor(false);
+        baraoVermelho.setAltura(0.0f);
+
+        System.out.println(
+            "Modelo: " + baraoVermelho.getModelo() +
+            "\nId: " + baraoVermelho.getIdentificador() +
+            "\nStatus Motor: " + baraoVermelho.getModelo() +
+            "\nAltura: " + baraoVermelho.getAltura()
+        );
+
+        System.out.println("----------------------------");
+        baraoVermelho.ligarMotor();
+        baraoVermelho.imprimeEstadoMotor();
+        baraoVermelho.desligarMotor();
+        baraoVermelho.imprimeEstadoMotor();
+        
+        System.out.println("----------------------------");
+        baraoVermelho.ligarMotor();
+        baraoVermelho.acelerar();
+        baraoVermelho.acelerar();
+        baraoVermelho.desligarMotor();
+        baraoVermelho.acelerar();
+
+        System.out.println("----------------------------");
+        baraoVermelho.ligarMotor();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desligarMotor();
+        baraoVermelho.desacelerar();
+        System.out.println("----------------------------");
+        baraoVermelho.ligarMotor();
+        baraoVermelho.acelerar();
+        baraoVermelho.acelerar();
+        baraoVermelho.acelerar();
+        baraoVermelho.acelerar();
+        baraoVermelho.acelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+        baraoVermelho.desacelerar();
+
+
+    }
+}
